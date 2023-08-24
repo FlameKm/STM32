f69de5a78a2a0181fa5b74299d0265c76245f705
hyc <417276783@qq.com>
2023-08-24 14:54:09 +0800

    complete dma for uart

--- 10_dma_uart_todo/src/main.c
+++ 10_dma_uart_todo/src/main.c
@@ -1,5 +1,6 @@
 
 
+#include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
@@ -10,7 +11,8 @@
 // 参考 https://github.com/intel/zephyr/blob/main/tests/drivers/uart/uart_async_api
 
 const struct device *uart_dev = DEVICE_DT_GET(DT_NODELABEL(usart1));
-
+K_SEM_DEFINE(sem_rdy, 0, 1);
+K_SEM_DEFINE(sem_rel, 0, 1);
 static void test_single_read_callback(const struct device *dev, struct uart_event *evt, void *user_data)
 {
     ARG_UNUSED(dev);
@@ -20,16 +22,14 @@ static void test_single_read_callback(const struct device *dev, struct uart_even
             printk("UART_TX_DONE\n");
             break;
         case UART_TX_ABORTED:
-            (*(uint32_t *)user_data)++;
             break;
-        case UART_RX_RDY:
-            printk("UART_RX_RDY\n");
+        case UART_RX_RDY: // !类似空闲中断
+            k_sem_give(&sem_rdy);
             break;
-        case UART_RX_BUF_RELEASED:
-            printk("UART_RX_BUF_RELEASED\n");
+        case UART_RX_BUF_RELEASED: // !结束数据完成
+            k_sem_give(&sem_rel);
             break;
         case UART_RX_DISABLED:
-            printk("UART_RX_DISABLED\n");
             break;
         default:
             break;
@@ -39,7 +39,7 @@ static void test_single_read_callback(const struct device *dev, struct uart_even
 volatile uint32_t tx_aborted_count;
 int main()
 {
-    uint8_t rx_buf[10] = {0};
+    uint8_t rx_buf[11] = {0};
     uint8_t tx_buf[] = "test\r\n";
 
     if (!device_is_ready(uart_dev)) {
@@ -54,9 +54,17 @@ int main()
     k_msleep(20);                                                  // wait transmit
     uart_tx(uart_dev, tx_buf, sizeof(tx_buf), 0);
     k_msleep(20);// wait transmit
+    uart_rx_enable(uart_dev, rx_buf, 10, 0);
     while (1) {
-        uart_rx_enable(uart_dev, rx_buf, 1, 1);
-        k_sleep(K_FOREVER);
+        k_sem_take(&sem_rdy, K_FOREVER);
+        printk("%s\n", rx_buf);
+        if (0 == k_sem_take(&sem_rel, K_MSEC(0))) {
+            rx_buf[10] = 0;
+            printk("\nrx buf: %s\n", rx_buf);
+            uart_rx_disable(uart_dev);
+            uart_rx_enable(uart_dev, rx_buf, 10, 0);
+            memset(rx_buf, 0, 11);
+        }
     }
     return 0;
 }

026cdc624ebf4dbb6b1b0d40a4f5b30d6b2e184b
hyc <417276783@qq.com>
2023-08-24 10:56:53 +0800

    uart

--- 07_uart/src/uart_poll.c
+++ 07_uart/src/uart_poll.c
@@ -1,3 +1,5 @@
+
+
 /*
  * Copyright (c) 2012-2014 Wind River Systems, Inc.
  *
@@ -9,27 +11,34 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
+/* 
+    输入什么就返回什么
+ */
+
 /** 
 * *这个的操作不是通过API操作，而是直接操作设备，这个和别的区别就是没有封装成DT（比如LED_GPIO）
-* *操作似乎比较底层，但是又不是特别底层
+* *操作似乎比较底层，但是又不是特别底层，是最基础的API实现的
 */
 
 int main(void)
 {
     int err;
+    char ret;
     // const struct device *uart = DEVICE_DT_GET(DT_PATH(soc, serial_40013800));
-    const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(usart1));
-    
-    err = device_is_ready(uart);
+    const struct device *uart_dev = DEVICE_DT_GET(DT_NODELABEL(usart1));
+
+    // 检查设备
+    err = device_is_ready(uart_dev);
     if (err < 0) {
-        return 0;
+        return -1;
     }
 
-    printk("this function is invalid, so not show this messge");
-    
     while (1) {
-        uart_poll_out(uart, 'c');
-        k_msleep(1000);
+        // POLL
+        err = uart_poll_in(uart_dev, &ret);
+        if (err >= 0) {
+            uart_poll_out(uart_dev, ret);
+        }
     }
 
     return 0;
